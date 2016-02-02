f4169e3eb59dc1298c46429cab71370592b5ddae
jonjozwiak <jjozwiak@redhat.com>
2016-02-02 15:53:24 -0600

    Adding Nova serialproxy example

---
+++ director-examples/serialproxy/README.md
@@ -0,0 +1,116 @@
+# Nova Serial Proxy for OSP7 Director
+This collection of templates shows how to setup nova-serialproxy and Serial Consoles on RHEL OSP 7 with Director.  Reference details as follows: 
+
+. https://blueprints.launchpad.net/horizon/+spec/serial-console
+. https://bugzilla.redhat.com/show_bug.cgi?id=974199
+. http://blog.oddbit.com/2014/12/22/accessing-the-serial-console-of-your-nova-servers/
+
+
+## Pre-requisites
+
+. A running/installed OSP Director host with heat templates copied to a working directory 
+``` 
+cp -rp /usr/share/openstack-tripleo-heat-templates /home/stack/templates
+```
+. A copy of the overcloud images in /home/stack/images 
+```
+# Files are accessible from:
+# https://access.redhat.com/downloads/content/191/ver=7/rhel---7/7/x86_64/product-software
+# Or https://access.redhat.com/downloads/ and download latest for RHEL OSP
+cd /home/stack/images
+for tarfile in *.tar; do tar -xf $tarfile; done
+```
+. A Local RHEL OSP 7 repo
+Assuming you have your Director host registered to RHN, you can reposync it as follows
+``` 
+# As Root - note this takes about 233mb
+yum -y install createrepo
+mkdir /var/www/html/repos
+reposync --gpgcheck -lnm --repoid=rhel-7-server-openstack-7.0-rpms --download_path=/var/www/html/repos
+createrepo /var/www/html/repos/rhel-7-server-openstack-7.0-rpms
+```
+
+## Image Updates
+The default overcloud images do not ship with the openstack-nova-serialproxy package.  I will show updating the image prior to upload.  However, you could also register the system with RHN or use local repos when provisioning the overcloud and install as a pre-step.  
+
+This step assumes you have a copy of the openstack-nova-serialproxy rpm in your /home/stack/images directory
+
+```
+# As stack user
+sudo yum -y install libguestfs-tools
+cd /home/stack/images
+
+# Ensure the IP matches your director provisioning IP address
+### Note this uses /etc/httpd/conf.d/10-horizon_vhost.conf
+cat << EOF >> /home/stack/images/rhelosp7temp.repo
+[rhel-7-server-openstack-7.0-rpms]
+name=rhel-7-server-openstack-7.0-rpms
+baseurl=http://192.168.220.10/html/repos/rhel-7-server-openstack-7.0-rpms/
+gpgcheck=0
+enabled=1
+EOF
+
+cp overcloud-full.qcow2 overcloud-full-orig.qcow2
+# Note - virt-customize can be run with '-v' for verbose
+virt-customize -a overcloud-full.qcow2 --upload rhelosp7temp.repo:/etc/yum.repos.d
+virt-customize -a overcloud-full.qcow2 --update 
+virt-customize -a overcloud-full.qcow2 --run-command 'yum -y install openstack-nova-serialproxy'
+
+guestfish -a overcloud-full.qcow2 -m /dev/sda
+cat /tmp/builder.log	# Will show output of the yum install
+ls /usr/bin   # check for nova-serialproxy....
+
+virt-customize -a overcloud-full.qcow2 --delete /etc/yum.repos.d/rhelosp7temp.repo
+
+# Update only images that have been changed
+source ~/stackrc
+openstack overcloud image upload --image-path /home/stack/images --update-existing
+```
+
+## Deploy your overcloud with serial console enabled
+
+. Deploy your overcloud and validate standard functionality
+. Create a subdirectory in your local templates directory `mkdir /home/stack/templates/custom`
+. Place all files in this repo in /home/stack/templates/custom
+```
+cd /home/stack
+git clone https://github.com/jonjozwiak/openstack.git
+cp openstack/director-examples/serialproxy/* /home/stack/templates/custom
+```
+. Execure the overcloud deploy with the new templates added:
+``` 
+-e /home/stack/templates/custom/nova-serialproxy-post-deploy.yaml
+```
+
+## Validate serial console functionality 
+The Nova serial console requires websockets and is not available just by telnet or ssh.  There is a simple python wrapper for this written by Lars Kellogg-Stedman which we will use.  Below shows the process to boot a server and connect to it's console.  This assumes your OpenStack environment has already been verified and has networks, images, and keypairs ready.  
+
+```
+# Boot instance and verify you can connect
+neutron net-list
+PRIVNET1ID=fb777762-4047-4f32-a1de-4367cfc546aa
+nova boot testserver --flavor m1.tiny --image cirros-0.3.4-x86_64 --key-name adminkey --security-groups default --nic net-id=$PRIVNET1ID
+FLOATINGIP=$(neutron floatingip-create pubnet1 | grep floating_ip_address | awk '{print $4}')
+nova add-floating-ip testserver $FLOATINGIP
+ping -c 1 $FLOATINGIP
+
+# Verify you get a web socket address for the instance
+nova get-serial-console testserver
+
+# Get and test a WebSockets Client
+cd /root
+git clone https://github.com/larsks/novaconsole.git
+cd novaconsole
+python setup.py install
+cd /root
+
+# Connect to the console
+novaconsole testserver
+```
+
+## Caveats
+Enabling Nova serialproxy results in the 'nova console-log' capability no longer functioning.  It is one or the other.  You will get an error like the following when trying to access the logs:
+
+ERROR (ClientException): The server has either erred or is incapable of performing the requested operation. (HTTP 500) (Request-ID: req-4f2fd582-9c6b-438e-a45b-76abe3b97ab5)
+
+
---
+++ director-examples/serialproxy/nova-serialproxy-config.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python
+import subprocess
+import socket
+import re
+
+def setup_serialproxy_controller():
+ haproxy_cfg_file = "/etc/haproxy/haproxy.cfg"
+ nova_internal_vip = cmd(["hiera", "tripleo::loadbalancer::internal_api_virtual_ip"])
+ public_virtual_ip = cmd(["hiera", "tripleo::loadbalancer::public_virtual_ip"])
+ controller_node_names = cmd(["hiera", "controller_node_names"])
+ nova_api_node_ips = cmd(["hiera", "nova_api_node_ips"])
+ nova_api_bind_address = cmd(["hiera", "nova::api::api_bind_address"])
+ compute_port_range = "10000:20000"
+ compute_proxyclient_address = cmd(["hiera", "nova::compute::vncserver_proxyclient_address"])
+ compute_proxy_host = cmd(["hiera", "nova::compute::vncproxy_host"])
+ 
+ # Remove unneeded characters from hieradata
+ nova_internal_vip = nova_internal_vip.translate(None, "\n")
+ public_virtual_ip = public_virtual_ip.translate(None, "\n")
+ nova_api_bind_address = nova_api_bind_address.translate(None, "\n")
+ compute_proxyclient_address = compute_proxyclient_address.translate(None, "\n")
+ compute_proxy_host = compute_proxy_host.translate(None, "\n")
+ controller_node_names = controller_node_names.translate(None, "[\"]\n") 
+ nova_api_node_ips = nova_api_node_ips.translate(None, "[\"]\n") 
+ controller_node_names = [controller_node_names]
+ nova_api_node_ips = [nova_api_node_ips]
+
+ # Prints per character
+ print "Controllers found:"
+ for (host,ip) in zip(controller_node_names, nova_api_node_ips):
+  print "host: ", host ,"; ip: ", ip
+
+ try:
+  print cmd(["yum", "-y", "install", "openstack-nova-serialproxy"])
+ except:
+  print "Yum install openstack-nova-serialproxy failed!"
+
+ print cmd(["openstack-config", "--set", "/etc/nova/nova.conf", "serial_console", "enabled", "true"])
+ print cmd(["openstack-config", "--set", "/etc/nova/nova.conf", "serial_console", "base_url", "ws://"+nova_api_bind_address+":6083"])
+
+ #test to see if haproxy.cfg has already been adjusted
+ proxy_cfg_adjusted = False
+ proxy_conf = open(haproxy_cfg_file,"r")
+ for line in proxy_conf:
+  if re.search('listen nova_serialproxy',line):
+   proxy_cfg_adjusted = True
+ proxy_conf.close()
+
+ if not proxy_cfg_adjusted:
+  proxy_conf = open(haproxy_cfg_file,"a")
+  proxy_conf.write("\n\n")
+  proxy_conf.write("listen nova_serialproxy\n")
+  proxy_conf.write("  bind %s:6083\n" % nova_internal_vip)
+  proxy_conf.write("  bind %s:6083\n" % public_virtual_ip)
+  for (host,ip) in zip(controller_node_names, nova_api_node_ips):
+    proxy_conf.write("  server %s %s:6083 check fall 5 inter 2000 rise 2\n" % (host, ip))
+  proxy_conf.close()
+ else:
+  print "INFO: Not adjusting haproxy.cfg since it appears to have the nova serialproxy config already"
+
+ no_firewall_rule = True
+ iptables_save_output = cmd(["iptables-save"])
+ for line in iptables_save_output.split('\n'):
+  if re.search('6083', line):
+   print "INFO: iptables rule already exists for port 6083"
+   no_firewall_rule = False
+
+ if no_firewall_rule:
+  print cmd(["iptables", "-I", "INPUT", "-p", "tcp", "-m", "multiport", "--dports", "6083", "-m", "comment", "--comment", "nova serialproxy incoming", "-j", "ACCEPT"])
+  try:
+   iptables_config_file = open("/etc/sysconfig/iptables","w")
+   iptables_save_output = cmd(["iptables-save"])
+   for line in iptables_save_output:
+    iptables_config_file.write(line)
+   iptables_config_file.close()
+  except:
+   print "persisting iptables rules failed!"
+
+ print cmd(["systemctl", "disable", "openstack-nova-serialproxy.service"])
+ print cmd(["systemctl", "stop", "openstack-nova-serialproxy.service"])
+
+ # Create Pacemaker Resources
+ no_pacemaker_resource = True
+ pcs_save_output = cmd(["pcs", "resource", "list"])
+ for line in pcs_save_output.split('\n'):
+  if re.search('openstack-nova-serialproxy', line):
+   print "INFO: Pacemaker resource "+line+" already exists"
+   no_pacemaker_resource = False
+ 
+ if no_pacemaker_resource:  
+  try:
+   print cmd(["pcs", "resource", "create", "openstack-nova-serialproxy", "systemd:openstack-nova-serialproxy", "--clone", "interleave=true"])
+   print cmd(["pcs", "constraint", "order", "start", "openstack-nova-consoleauth-clone", "then", "openstack-nova-serialproxy-clone"])
+   print cmd(["pcs", "constraint", "colocation", "add", "openstack-nova-serialproxy-clone", "with", "openstack-nova-consoleauth-clone"])
+  except:
+   print "pcs commands failed!"
+
+def setup_serialproxy_compute():
+ compute_port_range = "10000:20000"
+ compute_proxyclient_address = cmd(["hiera", "nova::compute::vncserver_proxyclient_address"])
+ compute_proxy_host = cmd(["hiera", "nova::compute::vncproxy_host"])
+
+ # Remove unneeded characters from hieradata
+ compute_proxyclient_address = compute_proxyclient_address.translate(None, "\n")
+ compute_proxy_host = compute_proxy_host.translate(None, "\n")
+
+ try:
+  print cmd(["yum", "-y", "install", "openstack-nova-serialproxy"])
+ except:
+  print "Yum install openstack-nova-serialproxy failed!"
+
+ print cmd(["openstack-config", "--set", "/etc/nova/nova.conf", "serial_console", "enabled", "true"])
+ print cmd(["openstack-config", "--set", "/etc/nova/nova.conf", "serial_console", "listen", "0.0.0.0"])
+ print cmd(["openstack-config", "--set", "/etc/nova/nova.conf", "serial_console", "proxyclient_address", compute_proxyclient_address])
+ print cmd(["openstack-config", "--set", "/etc/nova/nova.conf", "serial_console", "base_url", "ws://"+compute_proxy_host+":6083"])
+ print cmd(["openstack-config", "--set", "/etc/nova/nova.conf", "serial_console", "port_range", compute_port_range])
+
+
+ no_firewall_rule = True
+ iptables_save_output = cmd(["iptables-save"])
+ for line in iptables_save_output.split('\n'):
+  if re.search(compute_port_range, line):
+   print "INFO: iptables rule already exists for port "+compute_port_range
+   no_firewall_rule = False
+
+ if no_firewall_rule:
+  print cmd(["iptables", "-I", "INPUT", "-p", "tcp", "-m", "multiport", "--dports", compute_port_range, "-m", "comment", "--comment", "nova serialproxy incoming", "-j", "ACCEPT"])
+  try:
+   iptables_config_file = open("/etc/sysconfig/iptables","w")
+   iptables_save_output = cmd(["iptables-save"])
+   for line in iptables_save_output:
+    iptables_config_file.write(line)
+   iptables_config_file.close()
+  except:
+   print "persisting iptables rules failed!"
+
+ print cmd(["systemctl", "enable", "openstack-nova-serialproxy.service"])
+ print cmd(["systemctl", "start", "openstack-nova-serialproxy.service"])
+
+def cmd(args):
+    return subprocess.check_output(args)
+
+def main():
+ hostname = socket.gethostname()
+ if "controller" in hostname:
+  print("This is a controller node... continuing")
+  setup_serialproxy_controller()
+ elif "compute" in hostname:
+  print("This is a compute node... continuing")
+  setup_serialproxy_compute()
+ else:
+  print("Not running on a controller or compute node, so I'm not doing anything")
+
+if __name__ == '__main__':
+	main()
+
+
+
+
